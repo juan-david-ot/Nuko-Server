512bf9db3fae50e32a1b73f243736e3be9b5ede1
JuanDa <juandarm02@gmail.com>
2026-02-19 22:31:37 +0100

    Refactor code and configure scripts

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,9 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
-const PORT = 3000
-
 app.get('/', (req, res) => {
 	console.log('Hello World!!')
 	res.send('Hello World!!')
 })
 
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
-})
+export default app
