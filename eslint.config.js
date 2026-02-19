82a5bd98027e211a0b5d5bc3fdc1473a845c614d
JuanDa <juandarm02@gmail.com>
2026-02-19 20:53:43 +0100

    Add general project configuration

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,20 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['build', 'node_modules']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-    ],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: ['./tsconfig.app.json'],
-      },
-      ecmaVersion: 'latest',
-      globals: globals.node,
-    },
-  },
+	globalIgnores(['build', 'node_modules']),
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [js.configs.recommended, tseslint.configs.recommended],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				project: ['./tsconfig.json']
+			},
+			ecmaVersion: 'latest',
+			globals: globals.node
+		},
+		rules: {
+			'@typescript-eslint/no-unused-vars': 'warn'
+		}
+	}
 ])
--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,10 @@ app.use(express.json())
 const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!')
+	console.log('Hello World!!')
+	res.send('Hello World!!')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
