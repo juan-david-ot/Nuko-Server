c7c3999558906e84adfdcbcac928728cfd2dad18
JuanDa <juandarm02@gmail.com>
2026-02-20 11:15:56 +0100

    Update ports configuration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const PORT = process.env.PORT || 5005
+const PORT = process.env.PORT || 2608
 
 app.listen(PORT, () => {
 	console.log(`Server listening on http://localhost:${PORT}`)
