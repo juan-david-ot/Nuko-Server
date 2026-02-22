8dd79e5736aac4d76ee801b4c436f21dc162bcf9
JuanDa <juandarm02@gmail.com>
2026-02-22 20:25:59 +0100

    Refactor

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,20 +1,28 @@
 import { NextFunction, Request, Response } from 'express'
 import crypto from 'node:crypto'
-import { UserSchema } from '../models/user.schema'
+import { User, UserSchema } from '../models/user.schema'
 
 export async function test(req: Request, res: Response, next: NextFunction) {
-    const promise = UserSchema.safeParseAsync({ id: crypto.randomUUID(), name: '', nickname: 'test', email: 'test@example.com', password: 'password123' })
+
+    const userData: User = { id: crypto.randomUUID(), name: '', nickname: 'test', email: 'test@example.com', password: 'password123' }
+
+    const promise = UserSchema.safeParseAsync(userData)
+
+    console.log('Test!!')
 
     const response = await promise
 
     if (response.success) {
-        console.log('Validation successful:', response.data)
+        console.log('Validation successful:')
+
+        const newUser = response.data
+
+        console.log('New User:', newUser)
     }
     else if (!response.success) {
         console.error('Validation failed:', response.error.issues.map(issue => issue.message))
         next(new Error('Validation failed'))
     }
 
-    console.log('Test!!')
     res.send('Test!!')
 }
