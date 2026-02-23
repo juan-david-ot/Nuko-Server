b48ecd6ca18e4b2dfb77ac0a0dc892ba21ee615f
JuanDa <juandarm02@gmail.com>
2026-02-23 22:27:28 +0100

    WebSocket implementation

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,28 @@
+import { createServer } from 'node:http'
 import app from './app'
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 2608
 
-app.listen(PORT, () => {
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: process.env.ORIGIN || 'http://localhost:2409'
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('A NEW USER HAS CONNECTED')
+
+    socket.on('disconnect', () => {
+        console.log('a user has disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg)
+    })
+})
+
+server.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
 })
