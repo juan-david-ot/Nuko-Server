892c73201fa4865261d8780838c06f334897bb1f
JuanDa <juandarm02@gmail.com>
2026-03-06 14:02:37 +0100

    Refactor

---
+++ src/utils/string.utils.ts
@@ -0,0 +1,12 @@
+function stringUtilFunction1(): void {
+    console.log('stringUtilFunction1')
+}
+
+function stringUtilFunction2(): void {
+    console.log('stringUtilFunction2')
+}
+
+export {
+    stringUtilFunction1,
+    stringUtilFunction2
+}
