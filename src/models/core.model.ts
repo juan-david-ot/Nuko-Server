6c1f8d5630e311184f0d3089ca6c53f1fdc58d2a
JuanDa <juandarm02@gmail.com>
2026-03-17 00:08:58 +0100

    Cores model and schema

---
+++ src/models/core.model.ts
@@ -0,0 +1,15 @@
+import supabase from '../db'
+import { Core } from '../definitions/types'
+
+async function getCores() {
+    return supabase.from('cores').select()
+}
+
+async function saveCore(newCore: Core) {
+    return supabase.from('cores').insert(newCore).select()
+}
+
+export {
+    getCores,
+    saveCore
+}
---
+++ src/schemas/core.schema.ts
@@ -0,0 +1,18 @@
+import z from 'zod'
+
+const coreSchema = z.object({
+    id: z
+        .uuid({ error: 'ID must be a valid UUID' })
+        .optional(),
+    name: z
+        .string({ error: 'Name must be a string' })
+        .trim(),
+    creatorId: z.uuid({ error: 'ID must be a valid UUID' })
+})
+
+const partialCoreSchema = coreSchema.partial()
+
+export {
+    coreSchema,
+    partialCoreSchema
+}
