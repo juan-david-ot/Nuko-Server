3ff20b4763e90328ea4b201d9b1329861f5f0b33
JuanDa <juandarm02@gmail.com>
2026-03-19 00:08:51 +0100

    Add core router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,12 @@
 import { Express } from 'express'
 import authRouter from './auth.router'
+import coreRouter from './core.router'
 import helloWorlRouter from './hello-world.router'
 import testRouter from './test.router'
 
 export default (app: Express) => {
     app.use('/api/auth', authRouter)
+    app.use('/api/cores', coreRouter)
     app.use('/', helloWorlRouter)
     app.use('/test', testRouter)
 }
