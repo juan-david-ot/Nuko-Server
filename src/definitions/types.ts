592e2c32f2bc14a6d576cb50f33f22f396d1f93b
JuanDa <juandarm02@gmail.com>
2026-03-19 00:06:02 +0100

    Update types.ts

--- src/definitions/types.ts
+++ src/definitions/types.ts
@@ -2,6 +2,7 @@ import z from 'zod'
 import { partialUserSchema, userSchema } from '../schemas/user.schema'
 import { coreSchema, partialCoreSchema } from '../schemas/core.schema'
 import { coreUserSchema, partialCoreUserSchema } from '../schemas/core-user.schema'
+import { partialRoleSchema, roleSchema } from '../schemas/role.schema'
 
 export type User = z.infer<typeof userSchema>
 export type PartialUser = z.infer<typeof partialUserSchema>
@@ -9,5 +10,8 @@ export type PartialUser = z.infer<typeof partialUserSchema>
 export type Core = z.infer<typeof coreSchema>
 export type PartialCore = z.infer<typeof partialCoreSchema>
 
-export type UserCore = z.infer<typeof coreUserSchema>
-export type PartialUserCore = z.infer<typeof partialCoreUserSchema>
+export type Role = z.infer<typeof roleSchema>
+export type PartialRole = z.infer<typeof partialRoleSchema>
+
+export type CoreUser = z.infer<typeof coreUserSchema>
+export type PartialCoreUser = z.infer<typeof partialCoreUserSchema>
