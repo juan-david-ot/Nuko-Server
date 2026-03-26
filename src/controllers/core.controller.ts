4485509fbb4bf1239d95d151be9dd05a9700b4fe
JuanDa <juandarm02@gmail.com>
2026-03-26 22:32:10 +0100

    Change model import

--- src/controllers/core.controller.ts
+++ src/controllers/core.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 import { coreSchema } from '../schemas/core.schema'
-import CoreModel from '../models/core.model'
+import { CoreModel } from '../models'
 import { HttpError } from '../error-handler/http.error'
 
 async function getUserCores(req: Request, res: Response, next: NextFunction) {
