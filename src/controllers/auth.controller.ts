8a33cb7f87e0d4283641c433b0e3a62f8c52a6fe
JuanDa <juandarm02@gmail.com>
2026-03-26 22:30:34 +0100

    Change model import

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { partialUserSchema, userSchema } from '../schemas/user.schema'
-import UserModel from '../models/user.model'
+import { UserModel } from '../models'
 import { HttpError } from '../error-handler/http.error'
 
 async function signUp(req: Request, res: Response, next: NextFunction) {
