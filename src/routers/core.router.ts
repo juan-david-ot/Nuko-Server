dc948791d9cb0cc93e21ab470cfaae674837411e
JuanDa <juandarm02@gmail.com>
2026-03-31 18:17:16 +0200

    get core information endpoint

--- src/routers/core.router.ts
+++ src/routers/core.router.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import { verifyToken } from '../middlewares/auth.middleware'
-import { acceptInvitationToCore, createCore, createInvitationToCore, getUserCoreById, getUserCores } from '../controllers/core.controller'
+import { acceptInvitationToCore, createCore, createInvitationToCore, getUserCoreById, getUserCoreInformationById, getUserCores } from '../controllers/core.controller'
 
 const router = express.Router()
 
 router.get('/', verifyToken, getUserCores)
 
 router.get('/:id', verifyToken, getUserCoreById)
+
+router.get('/:id/information', verifyToken, getUserCoreInformationById)
 
 router.post('/', verifyToken, createCore)
 
