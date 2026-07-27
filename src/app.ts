baf554564b419b92b60a0f96e6ebd528537684dd
JuanDa <juandarm02@gmail.com>
2026-07-27 20:30:41 +0200

    Refactor

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import express from 'express'
 import 'dotenv/config'
 import './db'
 import config from './config'
-import routes from './routers'
+import routers from './routers'
 import errorHandler from './error-handler'
 
 const app = express()
 
 config(app)
-routes(app)
+routers(app)
 errorHandler(app)
 
 export default app
