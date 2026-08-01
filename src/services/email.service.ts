6acd233f1ff34a0379b02310da70da7eed46565e
JuanDa <77615084+juan-david-ot@users.noreply.github.com>
2026-08-01 22:53:29 +0200

    Update URL variable for email service

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -11,7 +11,7 @@ async function sendWelcomeEmail(sender: string, recipient: DBUser): Promise<Crea
             id: 'welcome-email',
             variables: {
                 username: recipient.username,
-                dashboard_url: 'http://localhost:2409/home'
+                dashboard_url: process.env.ORIGIN
             }
         }
     })
